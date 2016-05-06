fc0b79aad67283ab9c3a87346eeda22349711260
s14002 <s14002@std.it-college.ac.jp>
2016-05-06 19:52:49 +0900

    for Takara.

--- app/src/main/java/jp/ac/it_college/std/s14002/android/comikequizgame/StageSelect.java
+++ app/src/main/java/jp/ac/it_college/std/s14002/android/comikequizgame/StageSelect.java
@@ -55,28 +55,38 @@ public class StageSelect extends AppCompatActivity {
             int viewId = getResources().getIdentifier(resViewName, "id", getPackageName());
             Button button = (Button)findViewById(viewId);
             // Clearの値によって処理を分ける
-            if (ClearFlg[i -1] == 1) {
-                // Clearの値が1の場合
-                button.setText(String.valueOf(i)); // ボタンテキストに問題番号を表示
-                button.setTextColor(0xff33ffcc); //テキストの色
-                button.setBackgroundColor(0xffffffff); // ボタンの色
-                button.setTextSize(50); // テキストの文字のサイズ
-                button.setEnabled(true); // ボタン選択可能にする
-            } else {
-                // Clearの値が0の場合
-                button.setText(String.valueOf(i)); // ボタンテキストに問題番号を表示
-                button.setTextColor(0xffffffff); // テキストの色
-                button.setBackgroundColor(0xffb7b7b7); // ボタンの色
-                button.setTextSize(50); // テキストの文字のサイズ
-                button.setEnabled(false); // ボタン選択不可にする
-
+            if (i == 1) {
+                button.setText("1");
+            } else if (i == 2) {
+                button.setText("11");
+            } else if (i == 3) {
+                button.setText("21");
+            } else if (i == 4) {
+                button.setText("31");
+            } else if (i == 5){
+                button.setText("41");
+            } else if (i == 6) {
+                button.setText("51");
+            } else if (i == 7) {
+                button.setText("61");
+            } else if (i == 8) {
+                button.setText("71");
+            } else if (i == 9) {
+                button.setText("81");
+            } else if (i == 10) {
+                button.setText("91");
             }
+            button.setTextColor(0xff33ffcc); //テキストの色
+            button.setBackgroundColor(0xffffffff); // ボタンの色
+            button.setTextSize(50); // テキストの文字のサイズ
+            button.setEnabled(true); // ボタン選択可能にする
         }
+
     }
 
     // ボタンクリックでゲーム画面へ遷移
     public void onClick(View v) {
-        Intent intent = new Intent(StageSelect.this, MainGame.class);
+        Intent intent = new Intent(StageSelect.this, NormalGame.class);
         // 選択されたステージをボタンのテキストから取得
         intent.putExtra("QuestionNo", ((Button) v).getText());
         startActivity(intent);
